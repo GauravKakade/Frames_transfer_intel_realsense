63ada638bd40b196ef7058e3e2f28eae990d0563
Gaurav Kakade <70887509+GauravKakade@users.noreply.github.com>
2021-01-26 10:51:16 +0100

    Add files via upload

---
+++ Imagiz/sender.py
@@ -0,0 +1,16 @@
+import imagiz
+import cv2
+
+
+client=imagiz.Client("cc1",server_ip="localhost")
+vid=cv2.VideoCapture(6)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+    r,frame=vid.read()
+    if r:
+        r, image = cv2.imencode('.jpg', frame, encode_param)
+        client.send(image)
+    else:
+        break
+
